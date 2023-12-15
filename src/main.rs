a419bf9647b098171af6ef89265a6cc24b592fef
uku <uku3lig@gmail.com>
2023-12-15 10:48:02 +0100

    remove inputs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod common;
 mod solutions;
 
 fn main() {
+    let _ = dotenvy::dotenv();
+
     let args = std::env::args().collect::<Vec<_>>();
 
     let day = args
@@ -41,9 +43,17 @@ fn main() {
     println!("[*] Out: {} (took {})", out, format_time(time));
 }
 
-fn load(day: u8) -> std::io::Result<String> {
-    let path = format!("data/{:02}.txt", day);
-    std::fs::read_to_string(path).map(|s| s.trim().replace('\r', ""))
+fn load(day: u8) -> Result<String, Box<dyn std::error::Error>> {
+    let token = std::env::var("TOKEN").expect("TOKEN is not set");
+
+    Ok(
+        ureq::get(format!("https://adventofcode.com/2023/day/{day}/input").as_str())
+            .set("Cookie", format!("session={token}").as_str())
+            .call()?
+            .into_string()?
+            .trim()
+            .replace('\r', ""),
+    )
 }
 
 fn format_time(nanos: u128) -> String {
